962b60af5e38a1743c3d882e7f2b15360f378726
huanghuafeng <hhhhhf@foxmail.com>
2016-01-20 09:44:41 +0800

    修改README完善依赖包说明，增加install -l选项支持只做链接；

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 dotfiles backup with install &amp; update scripts
 
 # requiremets:
-zsh cscope ctags clang cmake python-dev
+git zsh cscope ctags clang cmake python-dev tmux
 build-essential libclang-dev libncurses-dev libboost-dev libz-dev cmake xz-utils
--- install.py
+++ install.py
@@ -26,11 +26,11 @@ def do_post_install(target_path):
     os.system("chsh -s /bin/zsh")
 
     # vim post installs
-    os.system("mkdir {0}/.vim/.backup".format(target_path))
-    os.system("mkdir {0}/.vim/.swap".format(target_path))
-    os.system("mkdir {0}/.vim/.undofiles".format(target_path))
+    os.system("mkdir -p {0}/.vim/.swap".format(target_path))
+    os.system("mkdir -p {0}/.vim/.undofiles".format(target_path))
     #os.system("git clone https://github.com/gmarik/vundle.git {0}/.vim/bundle/vundle".format(target_path))
-    os.system("git clone https://github.com/Shougo/neobundle.vim {0}/.vim/bundle/neobundle.vim".format(target_path))
+    #os.system("git clone https://github.com/Shougo/neobundle.vim {0}/.vim/bundle/neobundle.vim".format(target_path))
+    os.system('curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim')
 
     link_target= "{target_path}/.vimrc".format(target_path=target_path)
     linkname = "{target_path}/.vim/vimrc".format(target_path=target_path)
@@ -39,8 +39,8 @@ def do_post_install(target_path):
         os.system('mv {0} {1}'.format(link_target, link_target + '.' + postfix))
     os.system("ln -s {target_path}/.vim/vimrc {target_path}/.vimrc".format(target_path=target_path))
     #os.system("vim +PluginInstall +qall")
-    os.system("vim +NeoBundleInstall +qall")
-    #print('plugin install')
+    #os.system("vim +NeoBundleInstall +qall")
+    os.system("vim +PlugInstall +qall")
     #os.system("~/.vim/bundle/YouCompleteMe/install.py --clang-completer --omnisharp-completer --gocode-completer")
     #os.system("~/.vim/bundle/YouCompleteMe/install.py --clang-completer")
 
@@ -50,10 +50,12 @@ if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option("-f", "--fake", dest="fake",
             action="store_true", default=False, help="only print the actions")
-    parser.add_option("-p", "--post", dest="post",
+    parser.add_option("-p", "--postonly", dest="postonly",
             action="store_true", default=False, help="only do post install")
     parser.add_option("-t", "--target", dest="target",
             default=HOME_PATH, help="install target dir")
+    parser.add_option("-l", "--linkonly", dest="linkonly",
+            action="store_true", default=False, help="link dir only")
     (options, args) = parser.parse_args()
 
     fake_option = False
@@ -61,7 +63,9 @@ if __name__ == '__main__':
         fake_option = True
     target_path = options.target
 
-    if options.post:
+    if options.linkonly:
+        make_all_links(target_path, fake_option)
+    elif options.postonly:
         do_post_install(target_path)
     else:
         os.chdir(BASE_PATH)
